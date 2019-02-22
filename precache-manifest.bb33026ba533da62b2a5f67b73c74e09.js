487f4d8578bdc97660d3e11f09cfb38d87d9f74e
Ruosi Wang <ruosiwang.psy@gmail.com>
2019-02-22 13:45:29 -0500

    Updates

---
+++ precache-manifest.bb33026ba533da62b2a5f67b73c74e09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb0de9386edd863b9de5d4038e647e65",
+    "url": "/static/media/venues.cb0de938.jpg"
+  },
+  {
+    "revision": "c977db940e54aa529fb22725716d3469",
+    "url": "/static/media/topics.c977db94.jpg"
+  },
+  {
+    "revision": "a105d83955d2ad8bb3bf9f137c3ed35e",
+    "url": "/static/media/retention.a105d839.jpg"
+  },
+  {
+    "revision": "834ca3b02d13847cc06bf323a1555843",
+    "url": "/static/media/occurrence.834ca3b0.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "33320827e4fd453dbe62",
+    "url": "/static/js/main.33320827.chunk.js"
+  },
+  {
+    "revision": "00ec0959a47ed983999e",
+    "url": "/static/js/2.00ec0959.chunk.js"
+  },
+  {
+    "revision": "33320827e4fd453dbe62",
+    "url": "/static/css/main.c9e10fd0.chunk.css"
+  },
+  {
+    "revision": "07ea8aeda232442bdd74e3cc392ab645",
+    "url": "/index.html"
+  }
+];
